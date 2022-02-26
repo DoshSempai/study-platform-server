b87cb580355666442ba806063c975e9b77f4009c
Vlad <vlad8zavr@gmail.com>
2022-02-26 21:20:58 +0300

    [auth] Fix AuthGuard, getUserInfo

--- src/users/controller/users.controller.ts
+++ src/users/controller/users.controller.ts
@@ -69,7 +69,8 @@ export class UserController extends BaseController implements IUserController {
 		res: Response,
 		next: NextFunction,
 	): Promise<void> {
-		this.ok(res, { user: user });
+		const userInfo = await this.userService.getUserInfo(user);
+		this.ok(res, { email: userInfo?.email, id: userInfo?.id });
 	}
 
 	private signJWT(email: string, secret: string): Promise<string> {
